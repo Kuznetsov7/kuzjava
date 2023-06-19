9b9eb4ac07c4a07312c3e25c41b2ba5b032dcdcc
andrey <kuznetsov.1999.1999@gmail.com>
2023-06-19 09:48:12 +0300

    dz

---
+++ less1.java
@@ -0,0 +1,13 @@
+public class less1{
+    public static void main(String[] args) {
+        int sum = 0;
+        int num = 10;
+        for(int i = 1; i < num; i++)   
+        {
+            sum = sum + i;
+            System.out.println(sum);
+
+        }
+    }
+
+}
